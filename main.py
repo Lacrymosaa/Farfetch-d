36157fe43ea4a646163c8241614da5ad37bf60dc
Lapis <82874011+DiabolicWitch@users.noreply.github.com>
2022-10-02 20:21:29 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,29 +6,42 @@ import numpy as np
 import random
 from win32 import win32api, win32console
 
-time.sleep(3)
+time.sleep(2.5)
 
 def battle():
     pyautogui.press('1')
-    time.sleep(np.random.uniform(0.2,0.3))
+    time.sleep(np.random.uniform(0.3,0.4))
     pyautogui.press('1')
-    time.sleep(0.8)
+    time.sleep(0.93)
 
 def walk():
+    sametime = np.random.uniform(0.25, 0.40)
     pyautogui.keyDown('D')
-    time.sleep(np.random.uniform(0.6,0.7))
+    time.sleep(sametime + np.random.uniform(0.01, 0.05))
     pyautogui.keyUp('D')
-    time.sleep(np.random.uniform(0.1,0.3))
+    time.sleep(np.random.uniform(0.05,0.15)) #Interval of times between keypress
     pyautogui.keyDown('A')
-    time.sleep(np.random.uniform(0.6,0.7))
+    time.sleep(sametime + np.random.uniform(0.01, 0.05))
     pyautogui.keyUp('A')
 
  ### IMPORTANT: Confidence in this line can vary greatly. Just leaving it this way cannot guarantee that it will work.
  ### Another important thing is that Pok1.png not recommended that has no background###
-while True:
+while True: #823, 333
     walk()
     if pyautogui.locateOnScreen('nickname.png', grayscale=True, confidence=0.5) != None:
-        if pyautogui.locateOnScreen('Pok1.png', grayscale=False, confidence=0.38) != None:
-            quit()
-        time.sleep(1.8)
-        battle()
+        while pyautogui.locateOnScreen('nickname.png', grayscale=True, confidence=0.5) != None:
+            time.sleep(1.95)
+            if pyautogui.locateCenterOnScreen('solosis.png', grayscale=True, confidence=0.3, region=(770, 270, 120, 120)) != None:
+                print('I FOUND IT!')
+                quit()
+            battle()
+            if pyautogui.locateOnScreen('nickname.png', grayscale=True, confidence=0.5) != None:
+                time.sleep(3.5)
+                if pyautogui.locateOnScreen('nickname.png', grayscale=True, confidence=0.5) != None:
+                    pyautogui.click(x=580 ,y=532)
+                    print('I runned.')
+
+### nickname.png is a condition that will show to your program that battle started, but is not possible use moves. 
+# In this game i used the nickname bar that shows the battle started
+
+
